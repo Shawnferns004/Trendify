3eec8ca9aea916118cf9b9cd034fe2eb43ac06de
Shawnferns004 <shawnferns004@gmail.com>
2024-10-28 23:51:53 +0530

    error fix

--- server/server.js
+++ server/server.js
@@ -30,11 +30,8 @@ mongoose
 
 
 const app = express();
-<<<<<<< HEAD
-const PORT = process.env.PORT || 5000;
-=======
 const PORT = process.env.PORT || 4002;
->>>>>>> ea81859248473b7b0deb991bc69e9f5c709f04c6
+
 
 app.use(
   cors({
